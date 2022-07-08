a7991d961c665d54a629e3d5fa1bc2a73a95064f
Matheus Nunes Ismael <nunes.matheus.ismael@gmail.com>
2022-07-08 19:42:14 -0300

    Revert "Landing"

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
   getHello(): string {
-    return '🕸️MarvelHeroesAPI🕸️';
+    return this.appService.getHello();
   }
 }
